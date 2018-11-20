4c47f0c7a76058eba1021a4df52bb427f55ab840
skalmannen <alexwahlgren@hotmail.com>
2018-11-20 13:49:44 +0100

    Final commit

--- lab5/Word.java
+++ lab5/Word.java
@@ -4,10 +4,18 @@ public class Word {
 	
 	private String text;
 
+	/**
+	* Skapar ett nytt ord med den givna texten.
+	*/
 	public Word(String text) {
 		this.text = text;
 	}
 	
+	/**
+	* Jämför detta ord med det specificerade objektet. Resultatet är
+	* true om och endast om obj också är ett ord (Word) och har
+	* samma text.
+	*/
 	public boolean equals(Object obj) {
 		if (this == obj && this.toString() == obj.toString()) {
 			return true;
@@ -15,11 +23,17 @@ public class Word {
 		return false;
 	}
 	
+	/**
+	* Returnerar hashkoden för detta ord beräknat på ordets text.
+	*/
 	public int hashCode() {
 		return this.toString().hashCode();
 	}
 	
+	/**
+	* Returnerar texten för detta ord.
+	*/
 	public String toString() {
-		return text;
+		return this.text;
 	}
 }
--- lab5/WordQuiz.java
+++ lab5/WordQuiz.java
@@ -7,17 +7,43 @@ public class WordQuiz {
 	private Scanner input = new Scanner(System.in);
 	
 	public WordQuiz(Dictionary dictionary) {
-		
+		int missadeOrd = 0;
+		Dictionary missed = new Dictionary();
+		for (String key : dictionary.ordlista.keySet()) {
+			System.out.print(key + " = ");
+			String answer = input.nextLine();
+			if(!(answer.equals(dictionary.ordlista.get(key)))){
+				missed.add(key, dictionary.ordlista.get(key));
+				System.out.println("Fel!");
+				//lägg till antal försök gjorda
+				missadeOrd++;
+			}
+		}
+		if (missadeOrd != 0) {
+			System.out.println("Du missade "+missadeOrd+" ord! Jag förhör dig på dessa igen.");
+			WordQuiz retry = new WordQuiz(missed);
+			retry.runQuiz();
+		}else { System.out.println("Du klarade testet !!");}
 	 }
 	
 	public void runQuiz() {
-
+		
 	}
 	
 	public static void main(String[] args) {
 	
 		Dictionary sweng = new Dictionary();	
-		sweng.add
+		sweng.add("hej", "hello");
+		sweng.add("hej", "hi");
+		sweng.add("hej", "hey");
+		sweng.add("godnatt", "good night");
+		sweng.add("nattinatti", "good night");
+		sweng.add("fågel", "bird");
+		sweng.add("hund", "dog");
+		sweng.add("katt", "cat");
+		WordQuiz wq = new WordQuiz(sweng);
+		wq.runQuiz();
+
 	
 	}
 	
